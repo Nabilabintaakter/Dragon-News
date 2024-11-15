0c58ae2bd7ba2b137a350201ebef20bbb233b2d8
Nabilabintaakter <nabilabintaakter@gmail.com>
2024-11-15 16:21:20 +0600

    added register page layout

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
     return (
         <div className='flex justify-center items-center min-h-[90vh]'>
             <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-14">
-                <h2 className='text-3xl md:text-4xl font-semibold text-[#403F3F] text-center mb-3'>Login your account</h2>
+                <h2 className='text-3xl md:text-4xl font-bold text-[#403F3F] text-center mb-3'>Login your account</h2>
                 <div className="divider"></div>
                 <form className="card-body p-0">
                     <div className="form-control">
@@ -23,7 +23,7 @@ const Login = () => {
                         <button className="btn btn-neutral rounded-[5px] font-semibold text-xl">Login</button>
                     </div>
                 </form>
-                <p className='text-[#706F6F] font-semibold text-center'>Dont’t Have An Account ? <Link className='text-[#F9756F]' to='/auth/register'>Register</Link></p>
+                <p className='text-[#706F6F] font-semibold text-center'>Dont’t Have An Account ? Please <Link className='text-[#F9756F]' to='/auth/register'>Register</Link></p>
             </div>
         </div>
     );
--- src/layout/Register.jsx
+++ src/layout/Register.jsx
@@ -1,8 +1,50 @@
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
     return (
-        <div>register</div>
+        <div className='flex justify-center items-center min-h-[90vh]'>
+            <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-14">
+                <h2 className='text-3xl md:text-4xl font-bold text-[#403F3F] text-center mb-3'>Register your account</h2>
+                <div className="divider"></div>
+                <form className="card-body p-0">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold text-[#403F3F] text-xl">Your Name</span>
+                        </label>
+                        <input type="text" placeholder="Enter your name" className="input bg-[#F3F3F3] border-none rounded-[5px]" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold text-[#403F3F] text-xl">Photo URL</span>
+                        </label>
+                        <input type="text" placeholder="Enter your photo url" className="input bg-[#F3F3F3] border-none rounded-[5px]" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold text-[#403F3F] text-xl">Email</span>
+                        </label>
+                        <input type="email" placeholder="Enter your email address" className="input bg-[#F3F3F3] border-none rounded-[5px]" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold text-[#403F3F] text-xl">Password</span>
+                        </label>
+                        <input type="password" placeholder="Enter your password" className="input bg-[#F3F3F3] border-none rounded-[5px]" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label justify-start gap-3 cursor-pointer">
+                            <input type="checkbox" className="checkbox checkbox-sm md:checkbox-md" />
+                            <span className="label-text text-[#706F6F] font-semibold text-base">Accept Terms & Conditions</span>
+                        </label>
+                    </div>
+                    <div className="form-control my-6">
+                        <button className="btn btn-neutral rounded-[5px] font-semibold text-xl">Register</button>
+                    </div>
+                </form>
+                <p className='text-[#706F6F] font-semibold text-center'>Already Have An Account ? Please <Link className='text-[#F9756F]' to='/auth/login'>Login</Link></p>
+            </div>
+        </div>
     );
 };
 
